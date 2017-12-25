a3697450b8dcc28d8bc8ea017fb13e6b65afd481
Rongxin Sun <sunrongxin7666@163.com>
2017-12-25 22:47:37 +0800

    Spring Security 自定义配置

---
+++ uauth-security-browser/src/main/java/srx/awesome/code/security/browser/BrowserSecurityConfig.java
@@ -0,0 +1,17 @@
+package srx.awesome.code.security.browser;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter{
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()//表单认证
+                .and()
+                .authorizeRequests()//授权设置
+                .anyRequest()//任何请求
+                .authenticated();//都需要认证后授权
+    }
+}
